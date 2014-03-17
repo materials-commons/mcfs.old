7796a968cb383ddc0a2f1122929649e26a62ffba
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-03-17 16:01:05 -0400

    Update denorm tables.

--- service/rfiles.go
+++ service/rfiles.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/materials-commons/base/model"
 	"github.com/materials-commons/base/schema"
+	"github.com/materials-commons/mcfs"
 )
 
 // rFiles implements the Files interface for RethinkDB
@@ -33,14 +34,53 @@ func (f rFiles) Update(file *schema.DataFile) error {
 
 // Insert creates a new file entry.
 func (f rFiles) Insert(file *schema.DataFile) (*schema.DataFile, error) {
-	// TODO: Update denorm table with new file for directory
 	var newFile schema.DataFile
 	if err := model.Files.Q().Insert(file, &newFile); err != nil {
 		return nil, err
 	}
+	if err := f.insertIntoDenorm(&newFile); err != nil {
+		return &newFile, err
+	}
 	return &newFile, nil
 }
 
+// AddDirectories adds new directories to a file. It updates all related items
+// and join tables.
 func (f rFiles) AddDirectories(file *schema.DataFile, dirIDs ...string) error {
+	// Add directories to to datafile
+	for _, id := range dirIDs {
+		file.DataDirs = append(file.DataDirs, id)
+	}
+
+	if err := model.Files.Q().Update(file.ID, file); err != nil {
+		return mcfs.ErrDBUpdateFailed
+	}
+
+	// Add entries to the denorm table for this file
+	return f.insertIntoDenorm(file)
+}
+
+// insertIntoDenrom updates the denorm table with the new file entries.
+func (f rFiles) insertIntoDenorm(file *schema.DataFile) error {
+	fileEntry := schema.DataFileEntry{
+		ID:        file.ID,
+		Name:      file.Name,
+		Owner:     file.Owner,
+		Birthtime: file.Birthtime,
+		Checksum:  file.Checksum,
+		Size:      file.Size,
+	}
+
+	for _, ddirID := range file.DataDirs {
+		var ddirDenorm schema.DataDirDenorm
+		if err := model.DirsDenorm.Q().ByID(ddirID, &ddirDenorm); err != nil {
+			return err
+		}
+
+		ddirDenorm.DataFiles = append(ddirDenorm.DataFiles, fileEntry)
+		if err := model.DirsDenorm.Q().Update(ddirID, ddirDenorm); err != nil {
+			return err
+		}
+	}
 	return nil
 }
