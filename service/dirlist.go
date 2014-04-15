c9e50c33dd70cb686dea3a3044b064f0f4b51255
V. Glenn Tarcea <gtarcea@umich.edu>
2014-04-15 16:49:18 -0400

    Add in ID. Format and clean up code. Add comments.

--- service/dirlist.go
+++ service/dirlist.go
@@ -27,6 +27,7 @@ func (f fileList) Swap(i, j int) {
 }
 
 // Less determines whether one item is less than another for Sort.
+// We compare the lexographic order of the path.
 func (f fileList) Less(i, j int) bool {
 	return f[i].Path < f[j].Path
 }
@@ -35,32 +36,33 @@ func (f fileList) Less(i, j int) bool {
 // of DataDirDenorm items.
 func (dlist *dirList) build(denormEntries []schema.DataDirDenorm, base string) []dir.FileInfo {
 	for _, d := range denormEntries {
-		newDir := dir.FileInfo{
-			Path:  filepath.Join(base, d.Name),
-			MTime: d.Birthtime,
-			IsDir: true,
-		}
-		dlist.files = append(dlist.files, newDir)
-		for _, f := range d.DataFiles {
-			newFile := dir.FileInfo{
-				Path:     filepath.Join(base, d.Name, f.Name),
-				Size:     f.Size,
-				Checksum: f.Checksum,
-				MTime:    f.Birthtime,
-			}
-			dlist.files = append(dlist.files, newFile)
-		}
+		dlist.addDirectory(d, base)
 	}
 
 	sort.Sort(fileList(dlist.files))
 	return dlist.files
 }
 
+// addDirectory adds the directory and all its files to the list of entries.
+func (dlist *dirList) addDirectory(d schema.DataDirDenorm, base string) {
+	// Add the directory
+	newDir := dir.FileInfo{
+		ID:    d.ID,
+		Path:  filepath.Join(base, d.Name),
+		MTime: d.Birthtime,
+		IsDir: true,
+	}
+	dlist.files = append(dlist.files, newDir)
 
-
-
-
-
-
-
-
+	// Add directory's files
+	for _, f := range d.DataFiles {
+		newFile := dir.FileInfo{
+			ID:       f.ID,
+			Path:     filepath.Join(base, d.Name, f.Name),
+			Size:     f.Size,
+			Checksum: f.Checksum,
+			MTime:    f.Birthtime,
+		}
+		dlist.files = append(dlist.files, newFile)
+	}
+}
