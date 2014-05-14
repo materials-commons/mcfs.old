7c5262cb0ccdb8276e75355485d5b63ba0960431
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-14 15:24:04 -0400

    Add comments.

--- servers/recover/recover.go
+++ servers/recover/recover.go
@@ -2,8 +2,10 @@ package recover
 
 import ()
 
+// Request is what we have been asked to do.
 type Request struct{}
 
+// Response is something else
 type Response struct{}
 
 type recoveryServer struct {
@@ -14,10 +16,12 @@ var server = &recoveryServer{
 	request: make(chan Request, 50),
 }
 
+// Server returns the server
 func Server() *recoveryServer {
 	return server
 }
 
+// Run runs the server
 func (s *recoveryServer) Run(stopChan <-chan struct{}) {
 
 }
