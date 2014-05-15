50a8a78f12e04bdb4ff398dca86823c838b76245
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-15 10:13:12 -0400

    Remove the old code for createFile. Rename the createFileHandler2 type to createFileHandler. Remove some duplicate code in createNewFile and createNewFileVersion by consolidating it into newFile. To consolidate the code into newFile I had to make newFile a method off of createFileHandler so that it could access the files service member.

--- request/create_file.go
+++ request/create_file.go
@@ -1,16 +1,14 @@
 package request
 
 import (
-	"fmt"
 	r "github.com/dancannon/gorethink"
 	"github.com/materials-commons/base/mc"
-	"github.com/materials-commons/base/model"
 	"github.com/materials-commons/base/schema"
 	"github.com/materials-commons/mcfs/protocol"
 	"github.com/materials-commons/mcfs/service"
 )
 
-type createFileHandler2 struct {
+type createFileHandler struct {
 	user     string
 	dirs     service.Dirs
 	projects service.Projects
@@ -18,14 +16,14 @@ type createFileHandler2 struct {
 }
 
 func (h *ReqHandler) createFile(req *protocol.CreateFileReq) (resp *protocol.CreateResp, err error) {
-	cfh := newCreateFileHandler2(h.user)
+	cfh := newCreateFileHandler(h.user)
 
 	// Make sure we have a valid request.
 	if err := cfh.validateRequest(req, h.session); err != nil {
 		return nil, err
 	}
 
-	// Check if the file already exists.
+	// Check if the file status.
 	f, err := cfh.files.ByPath(req.Name, req.DataDirID)
 	switch {
 	case err == mc.ErrNotFound:
@@ -62,8 +60,8 @@ func (h *ReqHandler) createFile(req *protocol.CreateFileReq) (resp *protocol.Cre
 	}
 }
 
-func newCreateFileHandler2(user string) *createFileHandler2 {
-	return &createFileHandler2{
+func newCreateFileHandler(user string) *createFileHandler {
+	return &createFileHandler{
 		user:     user,
 		dirs:     service.NewDirs(service.RethinkDB),
 		projects: service.NewProjects(service.RethinkDB),
@@ -71,7 +69,7 @@ func newCreateFileHandler2(user string) *createFileHandler2 {
 	}
 }
 
-func (cfh *createFileHandler2) validateRequest(req *protocol.CreateFileReq, session *r.Session) error {
+func (cfh *createFileHandler) validateRequest(req *protocol.CreateFileReq, session *r.Session) error {
 	proj, err := cfh.projects.ByID(req.ProjectID)
 	if err != nil {
 		return mc.Errorf(mc.ErrInvalid, "Bad projectID %s", req.ProjectID)
@@ -101,13 +99,8 @@ func (cfh *createFileHandler2) validateRequest(req *protocol.CreateFileReq, sess
 	return nil
 }
 
-func (cfh *createFileHandler2) createNewFile(req *protocol.CreateFileReq) (*protocol.CreateResp, error) {
-	file := newFile(req, cfh.user)
-	dup, err := cfh.files.ByChecksum(file.Checksum)
-	if err == nil {
-		// Found a matching entry, set usesid to it
-		file.UsesID = dup.ID
-	}
+func (cfh *createFileHandler) createNewFile(req *protocol.CreateFileReq) (*protocol.CreateResp, error) {
+	file := cfh.newFile(req, cfh.user)
 	created, err := cfh.files.Insert(file)
 	if err != nil {
 		// Insert into database failed
@@ -121,15 +114,22 @@ func (cfh *createFileHandler2) createNewFile(req *protocol.CreateFileReq) (*prot
 	return &createResp, nil
 }
 
-func newFile(req *protocol.CreateFileReq, user string) *schema.File {
+func (cfh *createFileHandler) newFile(req *protocol.CreateFileReq, user string) *schema.File {
 	file := schema.NewFile(req.Name, user)
 	file.DataDirs = append(file.DataDirs, req.DataDirID)
 	file.Checksum = req.Checksum
 	file.Size = req.Size
+
+	dup, err := cfh.files.ByChecksum(file.Checksum)
+	if err == nil {
+		// Found a matching entry, set usesid to it
+		file.UsesID = dup.ID
+	}
+
 	return &file
 }
 
-func (cfh *createFileHandler2) partiallyUploaded(file *schema.File, mcdir string) bool {
+func (cfh *createFileHandler) partiallyUploaded(file *schema.File, mcdir string) bool {
 	id := datafileLocationID(file)
 	dfSize := datafileSize(mcdir, id)
 
@@ -139,14 +139,9 @@ func (cfh *createFileHandler2) partiallyUploaded(file *schema.File, mcdir string
 	return dfSize != file.Size
 }
 
-func (cfh *createFileHandler2) createNewFileVersion(file *schema.File, req *protocol.CreateFileReq) (*protocol.CreateResp, error) {
-	f := newFile(req, cfh.user)
+func (cfh *createFileHandler) createNewFileVersion(file *schema.File, req *protocol.CreateFileReq) (*protocol.CreateResp, error) {
+	f := cfh.newFile(req, cfh.user)
 	f.Parent = file.ID
-	dup, err := cfh.files.ByChecksum(file.Checksum)
-	if err == nil {
-		// Found a matching entry, set usesid to it
-		f.UsesID = dup.ID
-	}
 
 	// Hide the old file, but keep it around so we can get to it if needed.
 	cfh.files.Hide(file)
@@ -163,106 +158,4 @@ func (cfh *createFileHandler2) createNewFileVersion(file *schema.File, req *prot
 		ID: created.ID,
 	}
 	return &createResp, nil
-}
-
-type createFileHandler struct {
-	modelValidator
-}
-
-func (h *ReqHandler) createFileOld(req *protocol.CreateFileReq) (resp *protocol.CreateResp, err error) {
-	cfh := createFileHandler{
-		modelValidator: newModelValidator(h.user, h.session),
-	}
-
-	if err := cfh.validCreateFileReq(req); err != nil {
-		return nil, err
-	}
-
-	df := schema.NewFile(req.Name, h.user)
-	df.DataDirs = append(df.DataDirs, req.DataDirID)
-	df.Checksum = req.Checksum
-	df.Size = req.Size
-	otherID, err := cfh.duplicateFileID(req.Checksum, req.Size)
-	if err == nil && otherID != "" {
-		df.UsesID = otherID
-	}
-	rv, err := r.Table("datafiles").Insert(df).RunWrite(h.session)
-	if err != nil {
-		return nil, err
-	}
-
-	if rv.Inserted == 0 {
-		return nil, mc.ErrCreate
-	}
-	datafileID := rv.GeneratedKeys[0]
-
-	// TODO: Eliminate an extra query to look up the DataDir
-	// when we just did during verification.
-	datadir, _ := model.GetDirectory(req.DataDirID, h.session)
-	datadir.DataFiles = append(datadir.DataFiles, datafileID)
-
-	// TODO: Really should check for errors here. What do
-	// we do? The database could get out of sync. Maybe
-	// need a way to update partially completed items by
-	// putting into a log? Ugh...
-	r.Table("datadirs").Update(datadir).RunWrite(h.session)
-	createResp := protocol.CreateResp{
-		ID: datafileID,
-	}
-	return &createResp, nil
-}
-
-func (h createFileHandler) validCreateFileReq(fileReq *protocol.CreateFileReq) error {
-	proj, err := model.GetProject(fileReq.ProjectID, h.session)
-	if err != nil {
-		return fmt.Errorf("unknown project id %s", fileReq.ProjectID)
-	}
-
-	if proj.Owner != h.user {
-		return fmt.Errorf("user %s is not owner of project %s", h.user, proj.Name)
-	}
-
-	datadir, err := model.GetDirectory(fileReq.DataDirID, h.session)
-	if err != nil {
-		return fmt.Errorf("unknown datadir Id %s", fileReq.DataDirID)
-	}
-
-	if !h.datadirInProject(datadir.ID, proj.ID) {
-		return fmt.Errorf("datadir %s not in project %s", datadir.Name, proj.Name)
-	}
-
-	if h.datafileExistsInDataDir(fileReq.DataDirID, fileReq.Name) {
-		return mc.ErrExists
-	}
-
-	if fileReq.Size < 1 {
-		return fmt.Errorf("invalid size (%d) for datafile %s", fileReq.Size, fileReq.Name)
-	}
-
-	if fileReq.Checksum == "" {
-		return fmt.Errorf("bad checksum (%s) for datafile %s", fileReq.Checksum, fileReq.Name)
-	}
-
-	return nil
-}
-
-func (h *createFileHandler) duplicateFileID(checksum string, size int64) (id string, err error) {
-	rql := r.Table("datafiles").GetAllByIndex("checksum", checksum)
-	var datafiles []schema.File
-	err = model.GetRows(rql, h.session, &datafiles)
-	if err != nil {
-		return "", nil
-	}
-
-	for _, datafile := range datafiles {
-		if datafile.Size == size {
-			switch {
-			case datafile.UsesID == "":
-				return datafile.ID, nil
-			default:
-				return datafile.UsesID, nil
-			}
-		}
-	}
-	return "", nil
 }
