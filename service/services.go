a9b50dec7c8fb6114c746bed2ded82ee6182882f
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-05-17 13:33:31 -0400

    Refactor the code to simplify and move common code into a method. Add comments. Make use of the gohandy env package to get environment variables.

--- service/services.go
+++ service/services.go
@@ -3,7 +3,7 @@ package service
 import (
 	"fmt"
 	"github.com/materials-commons/base/db"
-	"os"
+	"github.com/materials-commons/gohandy/env"
 	"strings"
 )
 
@@ -25,30 +25,35 @@ var User Users
 // Setup the services in the init so that they can be configured to
 // the type of database to connect to.
 func init() {
-	dbType := getenv("MCDB_TYPE", "rethinkdb")
-	dbPort := getenv("MCDB_PORT", "30815")
-	dbName := getenv("MCDB_NAME", "materialscommons")
+	dbType := env.GetDefault("MCDB_TYPE", "rethinkdb")
 
 	switch strings.ToLower(dbType) {
 	case "rethinkdb":
-		conn := fmt.Sprintf("localhost:%s", dbPort)
-		db.SetAddress(conn)
-		db.SetDatabase(dbName)
-		File = NewFiles(RethinkDB)
-		Dir = NewDirs(RethinkDB)
-		Project = NewProjects(RethinkDB)
-		Group = NewGroups(RethinkDB)
-		User = NewUsers(RethinkDB)
+		setupRethinkDB()
 	default:
 		panic(fmt.Sprintf("Unsupported database type: %s", dbType))
 	}
 }
 
-func getenv(what, defaultValue string) string {
-	val := os.Getenv(what)
-	if val == "" {
-		return defaultValue
-	}
+// setupRethinkDB connects the services to rethinkdb. In a production setting
+// the environment variables should always be set. The defaults are set to
+// the test environment.
+func setupRethinkDB() {
+	dbPort := env.GetDefault("MCDB_PORT", "30815")
+	dbName := env.GetDefault("MCDB_NAME", "materialscommons")
+	conn := fmt.Sprintf("localhost:%s", dbPort)
 
-	return val
+	db.SetAddress(conn)
+	db.SetDatabase(dbName)
+	connectServices(RethinkDB)
+}
+
+// connectServices takes care of instantiating each of the services
+// to the correct service database. Add new services to this method.
+func connectServices(serviceDatabase ServiceDatabase) {
+	File = NewFiles(serviceDatabase)
+	Dir = NewDirs(serviceDatabase)
+	Project = NewProjects(serviceDatabase)
+	Group = NewGroups(serviceDatabase)
+	User = NewUsers(serviceDatabase)
 }
