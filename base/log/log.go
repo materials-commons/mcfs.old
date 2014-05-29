b0c2d31c5226748e1cc8103ae49758ee2818432b
V. Glenn Tarcea <gtarcea@umich.edu>
2014-05-29 11:44:53 -0400

    Switch logger over to log15 from logrus. Add utility functions.

--- base/log/log.go
+++ base/log/log.go
@@ -1,25 +1,46 @@
 package log
 
 import (
-	"github.com/Sirupsen/logrus"
-	"io"
+	"fmt"
+	"github.com/inconshreveable/log15"
+	"os"
 )
 
-// Global log variable.
-var L = logrus.New()
+var (
+	// Global log variable.
+	L = log15.New()
+
+	// Default handler used in the package.
+	defaultHandler log15.Handler
+)
 
 func init() {
-	L.Formatter = new(logrus.JSONFormatter)
+	stdoutHandler := log15.StreamHandler(os.Stdout, log15.LogfmtFormat())
+	L.SetHandler(log15.LvlFilterHandler(log15.LvlInfo, stdoutHandler))
 }
 
-// SetOutput sets the output for the global logger.
-func SetOutput(out io.Writer) {
-	L.Out = out
+// New creates a new instance of the logger using the current default handler
+// for its output.
+func New(ctx ...interface{}) log15.Logger {
+	l := log15.New(ctx)
+	l.SetHandler(defaultHandler)
+	return l
 }
 
-// Field is a convenience function to create a single field that is used in a file or package.
-func Field(name, value string) logrus.Fields {
-	return logrus.Fields{
-		name: value,
-	}
+// Msg is short hand to create a message string using fmt.Sprintf.
+func Msg(format string, args ...interface{}) string {
+	return fmt.Sprintf(format, args...)
+}
+
+// SetDefaultHandler sets the handler for the logger. It wraps handlers in a SyncHandler. You
+// should not pass in handlers that are already wrapped in a SyncHandler.
+func SetDefaultHandler(handler log15.Handler) {
+	defaultHandler = log15.SyncHandler(handler)
+	L.SetHandler(defaultHandler)
+}
+
+// DefaultHandler returns the current handler. It can be used to create additional
+// logger instances that all use the same handler for output.
+func DefaultHandler() log15.Handler {
+	return defaultHandler
 }
