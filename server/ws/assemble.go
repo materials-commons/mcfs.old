0ec886c0b0ea54b6b863fc2bef65378f99b3a82a
V. Glenn Tarcea <gtarcea@umich.edu>
2014-07-08 15:54:18 -0400

    Rework to move assembly into a separate type and methods. Clean up the code and comment it.

--- server/ws/assemble.go
+++ server/ws/assemble.go
@@ -4,24 +4,36 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"sort"
 	"strconv"
 
+	"github.com/inconshreveable/log15"
+	"github.com/materials-commons/mcfs/base/log"
 	"github.com/materials-commons/mcfs/base/mc"
 )
 
-func (r uploadResource) startAssemblers() {
-	for i := 0; i < maxAssemblers; i++ {
-		go r.fileAssembler()
+type assembler struct {
+	projectID   string
+	directoryID string
+	fileID      string
+	log         log15.Logger
+}
+
+func newAssembler(projectID, directoryID, fileID string) assembler {
+	return assembler{
+		projectID:   projectID,
+		directoryID: directoryID,
+		fileID:      fileID,
+		log:         log.New("go-routine", "assembler"),
 	}
 }
 
-func (r uploadResource) fileAssembler() {
-	for request := range r.assembleRequest {
-		r.assembleFile(request)
-	}
+func newAssemberFromFlowRequest(flowReq *FlowRequest) assembler {
+	return newAssembler(flowReq.ProjectID, flowReq.DirectoryID, flowReq.FileID)
 }
 
+// Sort the chunks by id. Each chunk is named by its chunk number.
 type byChunk []os.FileInfo
 
 func (c byChunk) Len() int      { return len(c) }
@@ -32,29 +44,34 @@ func (c byChunk) Less(i, j int) bool {
 	return chunkIName < chunkJName
 }
 
-func (r uploadResource) assembleFile(request finishRequest) {
-
-	// reassemble file
-	filePath := mc.FilePath(request.fileID)
+// assembleFile reassembles a file by combining all of its chunks. This routine
+// needs to ensure that all the chunks have arrived. It does this by counting
+// the number of entries in the directory vs the expected number
+func (a assembler) assembleFile() {
+	uploadPath := fileUploadPath(a.projectID, a.directoryID, a.fileID)
+	filePath := mc.FilePath(a.fileID)
 	fdst, err := os.Create(filePath)
 	if err != nil {
+		a.log.Error(log.Msg("Error creating assembly file %s: %s", filePath, err))
 		return
 	}
 	defer fdst.Close()
 
-	finfos, err := ioutil.ReadDir(request.uploadPath)
+	finfos, err := ioutil.ReadDir(uploadPath)
 	if err != nil {
+		a.log.Error(log.Msg("Error reading assembly dir %s: %s", uploadPath, err))
 		return
 	}
 
 	sort.Sort(byChunk(finfos))
 	for _, finfo := range finfos {
-		fsrc, err := os.Open(chunkPath(request.uploadPath, finfo.Name(), 1))
+		fsrc, err := os.Open(filepath.Join(uploadPath, finfo.Name()))
 		if err != nil {
+			a.log.Error(log.Msg("Error reading chunk %s: %s", finfo.Name(), err))
 			return
 		}
 		io.Copy(fdst, fsrc)
 		fsrc.Close()
 	}
-	os.RemoveAll(request.uploadPath)
+	os.RemoveAll(uploadPath)
 }
