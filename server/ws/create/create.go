1273dd5fbb44e8745d02ef4eec74b0268c9a4abf
V. Glenn Tarcea <gtarcea@umich.edu>
2014-07-08 16:47:12 -0400

    Start of the create set of web services.

---
+++ server/ws/create/create.go
@@ -0,0 +1,20 @@
+package create
+
+import "github.com/emicklei/go-restful"
+
+type createResource struct {
+}
+
+func NewResource(container *restful.Container) error {
+	createResource := createResource{}
+	createResource.register(container)
+	return nil
+}
+
+func (r createResource) register(container *restful.Container) {
+	ws := new(restful.WebService)
+
+	ws.Path("/create").Produces(restful.MIME_JSON)
+	ws.Route(ws.GET("/file").To(r.createFile).
+		Consumes(protocol.File{}))
+}
