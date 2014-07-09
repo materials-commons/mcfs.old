f1dfd6fd967852d98d2f22e420054ffa5b8403c1
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-07-09 19:36:55 -0400

    Fillout route methods.

--- server/ws/create/create.go
+++ server/ws/create/create.go
@@ -1,6 +1,9 @@
 package create
 
-import "github.com/emicklei/go-restful"
+import (
+	"github.com/emicklei/go-restful"
+	"github.com/materials-commons/base/protocol"
+)
 
 type createResource struct {
 }
@@ -15,6 +18,30 @@ func (r createResource) register(container *restful.Container) {
 	ws := new(restful.WebService)
 
 	ws.Path("/create").Produces(restful.MIME_JSON)
-	// ws.Route(ws.GET("/file").To(r.createFile).
-	// 	Consumes(protocol.File{}))
+	ws.Route(ws.PUT("/file").To(r.createFile).
+		Doc("Creates a new file or retrieves an existing one").
+		Reads(protocol.CreateFileReq{}).
+		Writes(protocol.CreateFileResp{}))
+	ws.Route(ws.PUT("/directory").To(r.createDirectory).
+		Doc("Creates a new directory or retrieves an existing one").
+		Reads(protocol.CreateDirectoryReq{}).
+		Writes(protocol.CreateDirectoryResp{}))
+	ws.Route(ws.PUT("/project").To(r.createProject).
+		Doc("Creates a new project or retrieves an existing one").
+		Reads(protocol.CreateProjectReq{}).
+		Writes(protocol.CreateProjectResp{}))
+
+	container.Add(ws)
+}
+
+func (r createResource) createFile(request *restful.Request, response *restful.Response) {
+
+}
+
+func (r createResource) createDirectory(request *restful.Request, response *restful.Response) {
+
+}
+
+func (r createResource) createProject(request *restful.Request, response *restful.Response) {
+
 }
