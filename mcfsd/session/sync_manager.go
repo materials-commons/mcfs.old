726dfba7ef633b2eb1583561f5eed659145a38d5
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2014-07-12 09:56:01 -0400

    Speculative code for timeouts.

--- mcfsd/session/sync_manager.go
+++ mcfsd/session/sync_manager.go
@@ -11,13 +11,14 @@ import (
 
 // ProjectSyncState tracks the current sync state for a project.
 type ProjectSyncState struct {
-	ProjectID string        // Project being synced
-	User      string        // User doing sync
-	Started   time.Time     // Time sync started
-	Uploaded  int64         // Bytes uploaded
-	LastItem  string        // Last item synced
-	TokenID   string        // Sync Token
-	Expires   time.Duration // When does the sync token expire
+	ProjectID   string        // Project being synced
+	User        string        // User doing sync
+	Started     time.Time     // Time sync started
+	Uploaded    int64         // Bytes uploaded
+	LastItem    string        // Last item synced
+	TokenID     string        // Sync Token
+	SawActivity bool          // Was there any activity on the sync?
+	Expires     time.Duration // When does the sync token expire
 }
 
 type syncSessionManager struct {
@@ -48,4 +49,36 @@ func (s *syncSessionManager) acquireSyncToken(user, project string) (string, err
 	}
 
 	return "", nil
+}
+
+func (s *syncSessionManager) expireSyncSession(project *ProjectSyncState) {
+	for {
+		project.expireSync()
+		s.mutex.Lock()
+		if !project.SawActivity {
+			break
+		}
+
+		// If we are here then there was sync activity between the
+		// expiration and the time we acquired the lock. So release
+		// lock and start waiting again.
+		s.mutex.Unlock()
+
+	}
+	// Perform cleanup and then release the lock.
+	s.mutex.Unlock()
+
+}
+
+func (p *ProjectSyncState) expireSync() {
+LOOP:
+	for {
+		select {
+		case <-time.After(15 * time.Second):
+			if !p.SawActivity {
+				break LOOP
+			}
+			p.SawActivity = false
+		}
+	}
 }
