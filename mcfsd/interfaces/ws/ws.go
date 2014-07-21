6046919d531f404807e790a48d4db8aee71e7b23
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-07-21 15:57:41 -0400

    Comment out old code to use as an example for what to do with the service.

--- mcfsd/interfaces/ws/ws.go
+++ mcfsd/interfaces/ws/ws.go
@@ -2,12 +2,11 @@ package ws
 
 import (
 	"github.com/emicklei/go-restful"
-	"github.com/materials-commons/mcfs/mcfsd/ws/upload"
 )
 
 // NewRegisteredServicesContainer creates a container for all the web services.
 func NewRegisteredServicesContainer() *restful.Container {
 	container := restful.NewContainer()
-	container.Add(upload.WebService())
+	//	container.Add(upload.WebService())
 	return container
 }
