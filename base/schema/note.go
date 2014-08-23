8bc212c2fb37a7f5e96a505c91e8d4f3f4049221
Glenn Tarcea <glenn.tarcea@gmail.com>
2014-08-22 20:54:38 -0400

    Move note

--- base/schema/note.go
+++
@@ -1,8 +0,0 @@
-package schema
-
-// Note represents a note entry.
-type Note struct {
-	Date    string `gorethink:"date"`
-	Message string `gorethink:"message"`
-	Who     string `gorethink:"who"`
-}
